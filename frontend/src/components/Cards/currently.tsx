e71132514f55c925e9ff8c75d64ee1915822ae5f
Serralog <marciolafourcade@gmail.com>
2022-11-28 09:29:50 -0300

    cards over chart

--- frontend/src/components/Cards/currently.tsx
+++ frontend/src/components/Cards/currently.tsx
@@ -4,128 +4,251 @@ import { Link } from "react-router-dom";
 
 // @mui material components
 import Card from "@mui/material/Card";
-import MuiLink from "@mui/material/Link";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Avatar,
+  Badge,
+  Box,
+  Grid,
+  LinearProgress,
+  Stack,
+  Tooltip,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
 
-export const Currently = (image: any, title: string) => {
-  return (
-    <Card>
-      <Box>
-        <Box>
-          Currently
-          {/*}
+type Locale = {
+  code?: string;
+  formatDistance?: (...args: Array<any>) => any;
+  formatRelative?: (...args: Array<any>) => any;
+  localize?: {
+    ordinalNumber: (...args: Array<any>) => any;
+    era: (...args: Array<any>) => any;
+    quarter: (...args: Array<any>) => any;
+    month: (...args: Array<any>) => any;
+    day: (...args: Array<any>) => any;
+    dayPeriod: (...args: Array<any>) => any;
+  };
+  formatLong?: {
+    date: (...args: Array<any>) => any;
+    time: (...args: Array<any>) => any;
+    dateTime: (...args: Array<any>) => any;
+  };
+  match?: {
+    ordinalNumber: (...args: Array<any>) => any;
+    era: (...args: Array<any>) => any;
+    quarter: (...args: Array<any>) => any;
+    month: (...args: Array<any>) => any;
+    day: (...args: Array<any>) => any;
+    dayPeriod: (...args: Array<any>) => any;
+  };
+  options?: {
+    weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+    firstWeekContainsDate?: 1 | 2 | 3 | 4 | 5 | 6 | 7;
+  };
+};
+
+// function subDays(date: Date | number, amount: number): Date;
+// namespace subDays {}
+
+// function formatDistance(
+//   date: Date | number,
+//   baseDate: Date | number,
+//   options?: {
+//     includeSeconds?: boolean;
+//     addSuffix?: boolean;
+//     locale?: Locale;
+//   }
+// ): string;
+// namespace formatDistance {}
+
+const DotLegend = styled("span")(
+  ({ theme }) => `
+    border-radius: 22px;
+    width: ${theme.spacing(1.5)};
+    height: ${theme.spacing(1.5)};
+    display: inline-block;
+    margin-right: ${theme.spacing(0.5)};
+    border: ${theme.colors.alpha.white[100]} solid 2px;
+`
+);
+
+const AvatarWrapper = styled(Avatar)(
+  ({ theme }) => `
+    width: ${theme.spacing(7)};
+    height: ${theme.spacing(7)};
+`
+);
+
+const LinearProgressWrapper = styled(LinearProgress)(
+  ({ theme }) => `
+    flex-grow: 1;
+    height: 10px;
     
-      
-        <Box
-          component="img"
-          src={image}
-          alt={title}
-          borderRadius="lg"
-          width="100%"
-          height="100%"
-          position="relative"
-          zIndex={1}
-        >
-          Aqui tem coisa
-        </Box>
-      </Box>
-  */}
-        </Box>
-      </Box>
-    </Card>
-  );
-};
+    &.MuiLinearProgress-root {
+      background-color: ${theme.colors.alpha.black[10]};
+    }
+    
+    .MuiLinearProgress-bar {
+      border-radius: ${theme.general.borderRadiusXl};
+    }
+`
+);
 
-//position="relative" borderRadius="lg" mt={-3} mx={2}
+// "Offline since " +
+// formatDistance(subDays(new Date(), 14), new Date(), {
+//   addSuffix: true,
+// })
 
-/*function SimpleBlogCard({
-  image: any,
-  title: string,
-  description: string,
-  action: any,
-}) {
+//<Text color="black">4</Text> out of <Text color="black">6</Text>
+
+export const Currently = () => {
+  const theme = useTheme();
   return (
-    <Card>
-      <Box position="relative" borderRadius="lg" mt={-3} mx={2}>
+    <Stack
+      direction={"row"}
+      // spacing={{ xs: 1, sm: 2, md: 4 }}
+      justifyContent="rigth"
+      alignItems="center"
+      sx={{ gap: 0.4, p: 0.4 }}
+    >
+      <Card>
         <Box
-          component="img"
-          src={image}
-          alt={title}
-          borderRadius="lg"
-          shadow="md"
-          width="100%"
-          height="100%"
-          position="relative"
-          zIndex={1}
-        />
-        <Box
-          borderRadius="lg"
-          shadow="md"
-          width="100%"
-          height="100%"
-          position="absolute"
-          left={0}
-          top="3%"
           sx={{
-            backgroundImage: `url(${image})`,
-            transform: "scale(0.94)",
-            filter: "blur(12px)",
-            backgroundSize: "cover",
+            m: theme.spacing(0, 3.9, 1.4, 3.9),
+            //height: (theme) => theme.spacing(3.9),
+            //width: (theme) => theme.spacing(20.9),
           }}
-        />
-      </Box>
-      <Box p={3}>
-        <Typography
-          display="inline"
-          variant="h3"
-          textTransform="capitalize"
-          fontWeight="bold"
         >
-          {title}
-        </Typography>
-        <Box mt={2} mb={3}>
-          <Typography variant="body2" component="p" color="text">
-            {description}
-          </Typography>
+          qualquer coisa!!
         </Box>
-        {action.type === "external" ? (
-          <MuiLink href={action.route} target="_blank" rel="noreferrer">
-            <Button color={action.color ? action.color : "dark"}>
-              {action.label}
-            </Button>
-          </MuiLink>
-        ) : (
-          <Link to={action.route}>
-            <Button color={action.color ? action.color : "dark"}>
-              {action.label}
-            </Button>
-          </Link>
-        )}
-      </Box>
-    </Card>
+      </Card>
+      <Card>
+        <Box
+          sx={{
+            height: (theme) => theme.spacing(3.9),
+            width: (theme) => theme.spacing(20.9),
+          }}
+        >
+          Outra coisa
+        </Box>
+      </Card>
+      <Card>
+        <Box
+          sx={{
+            height: (theme) => theme.spacing(3.9),
+            width: (theme) => theme.spacing(20.9),
+          }}
+        >
+          Nova coisa
+        </Box>
+      </Card>
+      <Card>
+        <Box
+          sx={{
+            height: (theme) => theme.spacing(3.9),
+            width: (theme) => theme.spacing(20.9),
+          }}
+        >
+          Nova coisa
+        </Box>
+      </Card>
+      <Card>
+        <Box
+          sx={{
+            height: (theme) => theme.spacing(3.9),
+            width: (theme) => theme.spacing(20.9),
+          }}
+        >
+          Nova coisa
+        </Box>
+      </Card>
+      <Card>
+        <Box
+          sx={{
+            height: (theme) => theme.spacing(3.9),
+            width: (theme) => theme.spacing(20.9),
+          }}
+        >
+          Nova coisa
+        </Box>
+      </Card>
+    </Stack>
   );
-}
-
-// Typechecking props for the SimpleBlogCard
-SimpleBlogCard.propTypes = {
-  image: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  action: PropTypes.shape({
-    type: PropTypes.oneOf(["external", "internal"]).isRequired,
-    route: PropTypes.string.isRequired,
-    color: PropTypes.oneOf([
-      "primary",
-      "secondary",
-      "info",
-      "success",
-      "warning",
-      "error",
-      "dark",
-      "light",
-      "default",
-    ]),
-    label: PropTypes.string.isRequired,
-  }).isRequired,
 };
+
+/*
+        <Stack direction={"row"}>
+          <Box width="10%" height="100%">
+            <Card>qualquer coisa</Card>
+          </Box>
+          <Box width="10%" height="100%">
+            <Card>qualquer coisa</Card>
+          </Box>
+        </Stack>
 */
+
+/*<Stack direction={"column"}>
+
+</Stack>
+  </Box>
+  <Box width="100%" height="100%">
+<Stack direction={"column"}>
+
+</Stack>*/
+
+// export const Currently = (image: any, title: string) => {
+//   const theme = useTheme();
+//   return (
+//     <Grid item xs={12} md={4}>
+//       <Card sx={{ p: 2.5 }}>
+//         <Box display="flex" alignItems="center" pb={3}>
+//           <Badge
+//             anchorOrigin={{
+//               vertical: "bottom",
+//               horizontal: "right",
+//             }}
+//             overlap="circular"
+//             badgeContent={
+//               <Tooltip arrow placement="top" title={"Offline since "}>
+//                 <DotLegend
+//                   style={{ background: `${theme.colors.error.main}` }}
+//                 />
+//               </Tooltip>
+//             }
+//           >
+//             <AvatarWrapper
+//               alt="Remy Sharp"
+//               src="/static/images/avatars/4.jpg"
+//             />
+//           </Badge>
+//           <Box sx={{ ml: 1.5 }}>
+//             <Typography variant="h4" noWrap gutterBottom>
+//               Hanna Siphron
+//             </Typography>
+//             <Typography variant="subtitle2" noWrap>
+//               Web Dev Support Team
+//             </Typography>
+//           </Box>
+//         </Box>
+
+//         <Typography variant="subtitle2" gutterBottom>
+//           tasks completed
+//         </Typography>
+//         <LinearProgressWrapper
+//           value={65}
+//           color="primary"
+//           variant="determinate"
+//         />
+//       </Card>
+//     </Grid>
+
+//     // <Card>
+//     //   <Box>
+//     //     <Box>
+//     //       Currently
+//     //     </Box>
+//     //   </Box>
+//     // </Card>
+//   );
+// };
